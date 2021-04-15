bfebd267dc0d1f746bd15a40733fb89e057711b3
Lea23VC <leandro.villalobos.dx@gmail.com>
2021-04-14 23:47:18 -0400

    :bento: New image logo, favicon and fonts!
    Tons of new shit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,53 @@
 import Head from 'next/head'
 import React, { useEffect, useState } from "react";
 import Typography from '@material-ui/core/Typography'
-import {useSpring, animated} from 'react-spring'
+import { useSpring, animated } from 'react-spring'
 import { makeStyles } from '@material-ui/core/styles';
 
 import Test from '../containers/test'
 import Instagram from '../containers/instagram'
 import Grid from '@material-ui/core/Grid';
 
+import Image from 'next/image'
 
 
+const useStyles = makeStyles((theme) => ({
 
-const useStyles = makeStyles((theme) => ({ 
+  title: {
 
-  test: { 
-    
-    
-    
+    fontFamily: 'Supernett-cn',
+    color: "white",
+    '&:first-Letter': { //ASI SE USA SOLO LA PRIMERA LETRA AAAAA
+
+
+      fontSize: "180%"
+    },
+
+
+  },
+
+  subtitle: {
+
+    fontFamily: 'Supernett-cn',
+    color: "white",
+
+  },
+
+  a: {
+    display: "inline-block",
+    '&:first-Letter': { //ASI SE USA SOLO LA PRIMERA LETRA AAAAA
+
+
+      fontSize: "180%"
+    },
+
+  },
+
+  logo: {
+
+
   }
+
 
 
 
@@ -27,42 +57,84 @@ const useStyles = makeStyles((theme) => ({
 export default function Home() {
 
   const classes = useStyles();
-  const props = useSpring({opacity: 1, from: {opacity: 0}})
+  const props = useSpring({ opacity: 1, from: { opacity: 0 }, config: { duration: 3000 } })
   const propsDonut = useSpring({ value: 100, from: { value: 0 } })
   const [flipped, set] = useState(false)
-  const { transform, opacity } = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-    config: { mass: 5, tension: 500, friction: 80 }
-  })
+
 
   return (
-    
+
     <>
-    
-    <div className={classes.test}>
-    <animated.div style={props}><Typography align="center" variant="h1" >Aldo Cabrera A.</Typography>
-    
-    <Typography align="center" variant="h2" >Animador Digital / Ilustrador</Typography>
-    
-    </animated.div>
+
+      <Head>
+
+        <link rel="shortcut icon" href="/images/favicon.png" />
+        <link
+          rel="preload"
+          href="/fonts/Supernett-cn/Supernettcn-Condensed.otf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Supernett-cn/Supernettcn-LightCondensed.otf"
+          as="font"
+          crossOrigin=""
+        />
+
+        <link
+          rel="preload"
+          href="/fonts/Supernett-cn/Supernettcn-BoldCondensed.otf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
 
 
-    <Grid container> 
-      <Grid item align="center" xs={12} lg={12} >
-      <Test/>
-      </Grid>
-      
-      <Instagram/>
-      
-    </Grid>
 
 
-    
+      <div className={classes.test}>
+        <animated.div style={props}><Typography className={classes.title} align="center" variant="h1" >Aldo <div className={classes.a}>Cabrera</div> <div className={classes.a}>A.</div></Typography>
 
-   
-    
-    </div>
+          <Typography className={classes.subtitle} align="center" variant="h2" >Animador Digital / Ilustrador</Typography>
+
+
+
+
+
+        </animated.div>
+
+
+
+        <Grid container justify="center">
+          <Grid className={classes.logo} item >
+            <animated.div style={props}>
+              <Image
+
+                src="/images/logo_nyaldox2.png"
+                alt="Picture of the author"
+                width={761 * 0.7}
+                height={807 * 0.7}
+              />
+            </animated.div>
+
+          </Grid>
+
+
+          <Grid item align="center" xs={12} lg={12} >
+            <Test />
+          </Grid>
+
+          <Instagram />
+
+        </Grid>
+
+
+
+
+
+
+      </div>
 
     </>
   )
